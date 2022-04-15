41462f5b9a15f550188817d329049e2e891ea1b2
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-15 16:43:15 +0500

    add todo

--- js/app.js
+++ js/app.js
@@ -1,64 +1,98 @@
 const app = new Vue({
-    el:"#app",
-    data(){
+    el: "#app",
+    data() {
         return {
-            showTodos:false,
-            showHome:true,
-            showForm:false,
+            showTodos: false,
+            showHome: true,
+            showForm: false,
             // Add todo 
-            title:"",
-            description:"",
-            status:false,
-            priority:"",
-            added:"00:00",
-            todos:[
-                {
-                    id:1,
-                    title:"Play Game",
-                    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo maiores iste dolores mollitia? Dolor, quam.",
-                    status:true,
-                    priority:'is-danger',
-                    added:"16:23:38"
+            title: "",
+            description: "",
+            status: false,
+            priority: "",
+            added: "00:00",
+            todos: [{
+                    id: 1,
+                    title: "Play Game",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo maiores iste dolores mollitia? Dolor, quam.",
+                    priority: "is-info",
+                    date: "13:00",
+                    status: false
                 },
-
-            ]
+                {
+                    id: 2,
+                    title: "Play PS5",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo maiores iste dolores mollitia? Dolor, quam.",
+                    priority: "is-primary",
+                    date: "11:00",
+                    status: false
+                },
+                {
+                    id: 3,
+                    title: "Play Hard",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo maiores iste dolores mollitia? Dolor, quam.",
+                    priority: "is-danger",
+                    date: "15:00",
+                    status: false
+                }
+            ],
+            doned: []
         }
     },
-    methods:{
-        showTodoList(){
+    methods: {
+        showTodoList() {
             this.showHome = false;
             this.showForm = false;
             this.showTodos = true;
         },
-        showAddForm(){
+        showAddForm() {
             this.showHome = false;
             this.showForm = true;
             this.showTodos = false;
         },
+        getPriority(todo) {
+            return todo.priority
+        },
         // ADD TODO 
-        addTodo(){
-            this.todos.push(
-                {
-                    id:this.getTodosCount + 1,
-                    title:this.title,
-                    description:this.description,
-                    status:false,
-                    priority:this.priority,
-                    added:this.getTimeNow
-                }
-            );
+        addTodo() {
+
+            this.todos.push({
+                id: this.getTodosCount + 1,
+                title: this.title,
+                description: this.description,
+                status: false,
+                priority: this.priority,
+                added: this.getTimeNow
+            });
             return this.showTodoList()
-        }
+        },
+        // Complete TODO 
+        todoDone(todo) {
+            todo.status = true
+            this.doned.push(todo)
+            this.todos.splice(this.todos.indexOf(todo), 1)
+        },
+        // DELETE TODO 
+        deleteTodo(todo) {
+            this.todos.splice(this.todos.indexOf(todo), 1)
+        },
+        // DELETE DONED TODO 
+        deleteDonedTodo(todo) {
+            this.doned.splice(this.todos.indexOf(todo), 1)
+        },
     },
-    computed:{
-        getTodosCount(){
+    computed: {
+        getTodosCount() {
             return this.todos.length
         },
-        getTimeNow(){
+        getDonedTodosCount() {
+            return this.doned.length
+        },
+        getTimeNow() {
             return new Date().toLocaleTimeString();
-        }
+        },
+        // getPriority(todo) {
+        //     return todo.priority
+        // }
     }
 });
-
-let data = new Date();
-console.dir(data.toLocaleTimeString())
